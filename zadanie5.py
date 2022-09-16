09eba1852f989008782fc2f07d1c9aa51d7b4a72
Gibeon228 <106450724+Gibeon228@users.noreply.github.com>
2022-09-17 01:02:12 +0300

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,5 +1,8 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 def Niga_fibonachi(n):
-
     if (n<-2):
         return Niga_fibonachi(n+2) - Niga_fibonachi(n+1)
     
@@ -17,4 +20,5 @@ def Niga_fibonachi(n):
 
 print ("Введите число n")
 n = int(input())
-print(Niga_fibonachi(n))
+for i in range (-1*n, n+1):
+    print(Niga_fibonachi(i))
